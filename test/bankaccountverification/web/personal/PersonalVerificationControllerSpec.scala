d336f08ed181c4f9e124fe88a61fa42ac0f4a5de
Pete Smith <pete.smith@digital.hmrc.gov.uk>
2020-09-22 11:24:53 +0100

    [PS|SS] Fixed failing test (again)

--- test/bankaccountverification/web/personal/PersonalVerificationControllerSpec.scala
+++ test/bankaccountverification/web/personal/PersonalVerificationControllerSpec.scala
@@ -415,7 +415,7 @@ class PersonalVerificationControllerSpec extends AnyWordSpec with Matchers with 
         contentAsString(result) should include("some account name")
         contentAsString(result) should include("112233")
         contentAsString(result) should include("12345678")
-        contentAsString(result) should include("We have not been able to check the account details you entered. Make sure the details you entered are correct.")
+        contentAsString(result) shouldNot include("$bankName$")
       }
 
       "confirmation view is rendered correctly with a bank name" in {
@@ -439,8 +439,7 @@ class PersonalVerificationControllerSpec extends AnyWordSpec with Matchers with 
         contentAsString(result) should include("some account name")
         contentAsString(result) should include("112233")
         contentAsString(result) should include("12345678")
-        contentAsString(result) should include("We have not been able to check the account details you entered with sort-code-bank-name-personal. Make sure the details you entered are correct.")
-      }
+        contentAsString(result) should include("with sort-code-bank-name-business")}
     }
   }
 }
