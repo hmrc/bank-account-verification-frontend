56e28dfc4b03e4926ee7e36910592030ec9203fe
Pete Smith <pete.smith@digital.hmrc.gov.uk>
2020-11-02 15:37:12 +0000

    [PS] TAV-157 - Update dependencies including play-language to fix open redirect issue

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -5,15 +5,15 @@ object AppDependencies {
 
   val compile = Seq(
     "uk.gov.hmrc" %% "simple-reactivemongo" % "7.30.0-play-27",
-    "uk.gov.hmrc" %% "bootstrap-frontend-play-27" % "2.24.0",
+    "uk.gov.hmrc" %% "bootstrap-frontend-play-27" % "3.0.0",
     "uk.gov.hmrc" %% "play-frontend-hmrc" % "0.20.0-play-27",
-    "uk.gov.hmrc" %% "play-frontend-govuk" % "0.49.0-play-27",
+    "uk.gov.hmrc" %% "play-frontend-govuk" % "0.53.0-play-27",
     "uk.gov.hmrc" %% "play-partials" % "6.11.0-play-27",
-    "uk.gov.hmrc" %% "play-language" % "4.3.0-play-27"
+    "uk.gov.hmrc" %% "play-language" % "4.4.0-play-27"
   )
 
   val test = Seq(
-    "uk.gov.hmrc" %% "bootstrap-test-play-27" % "2.24.0" % Test,
+    "uk.gov.hmrc" %% "bootstrap-test-play-27" % "3.0.0" % Test,
     "org.scalatest" %% "scalatest" % "3.1.2" % Test,
     "org.jsoup" % "jsoup" % "1.10.2" % Test,
     "com.typesafe.play" %% "play-test" % current % Test,
