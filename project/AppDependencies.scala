d9342819663f10e1b099144cd156aa8af1808aa7
grahampaulcook <11269301+grahampaulcook@users.noreply.github.com>
2025-08-05 08:45:56 +0100

    CIR-2230 platops-pr-bot suggestions

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -8,7 +8,7 @@ object AppDependencies {
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"         % hmrcMongoPlayVersion,
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30" % bootstrapPlayVersion,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30" % "12.7.0",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30" % "12.8.0",
     "uk.gov.hmrc"       %% "play-partials-play-30"      % "10.1.0"
   )
 
