8f82e384ad79e82d83f996ccda44a4890c3f0c47
grahampaulcook <11269301+grahampaulcook@users.noreply.github.com>
2026-04-15 12:39:48 +0100

    CIR-2916 platops-pr-bot suggestion applied

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,13 +3,14 @@ import sbt.*
 object AppDependencies {
 
   private val bootstrapPlayVersion = "10.7.0"
+  private val playFrontendVersion  = "12.32.0"
   private val hmrcMongoPlayVersion = "2.12.0"
   private val playSuffix           = "-play-30"
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc.mongo" %% s"hmrc-mongo$playSuffix"         % hmrcMongoPlayVersion,
     "uk.gov.hmrc"       %% s"bootstrap-frontend$playSuffix" % bootstrapPlayVersion,
-    "uk.gov.hmrc"       %% s"play-frontend-hmrc$playSuffix" % "12.29.0",
+    "uk.gov.hmrc"       %% s"play-frontend-hmrc$playSuffix" % playFrontendVersion,
     "uk.gov.hmrc"       %% s"play-partials$playSuffix"      % "10.2.0"
   )
 
